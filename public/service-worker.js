fddf58a50a31137684e5c0a5ad475c711c8cf309
Edgar Ortega <edort93@gmail.com>
2021-12-07 23:25:00 -0600

    updated path to manifest

--- public/service-worker.js
+++ public/service-worker.js
@@ -11,7 +11,7 @@ const FILES_TO_CACHE = [
     '/icons/icon-512x512.png',
     'https://fonts.googleapis.com/css?family=Istok+Web|Montserrat:800&display=swap',
     'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css',
-    "/manifest.webmanifest"
+    "/manifest.json"
   ];
   
  
